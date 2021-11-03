5a37df0b62e74c6fe2c4e63d62ddcbc060d22b45
Alyona <alyonagerasimova@gmail.com>
2021-11-03 17:13:18 +0400

    Upd Hometask19

--- Task19/my-app/src/App.tsx
+++ Task19/my-app/src/App.tsx
@@ -1,9 +1,50 @@
 import './App.css';
+import './module/components/forms.css';
 import React, {useState} from "react";
 import {ThemeContext, themes} from "./module/theme-context";
 import {AnimalsMenu} from './module/components/AnimalsMenu';
+import {AnimalCreate} from "./module/components/AnimalCreate";
+import {
+    BrowserRouter as Router,
+    Switch,
+    Route,
+    Link
+} from "react-router-dom";
 
 export function App() {
+
+    return (
+        <Router>
+            <div>
+                <Link to="/">Список животных</Link>
+            </div>
+            <div>
+                <Link to="/create">Добавить животного</Link>
+            </div>
+            <div>
+                <Link to="/edit">Редактировать животного</Link>
+            </div>
+
+            <Switch>
+                <Route exact path="/">
+                    <Home/>
+                </Route>
+                <Route exact path="/create">
+                    <div className="container">
+                        <div className="content">
+                            <AnimalCreate/>
+                        </div>
+                    </div>
+                </Route>
+                <Route exact path="/edit">
+                    <Home/>
+                </Route>
+            </Switch>
+        </Router>
+    );
+}
+
+function Home() {
 
     const [theme, setTheme] = useState(themes.light);
     const toggleTheme = () => {
--- Task19/my-app/src/module/components/AnimalCreate.tsx
+++ Task19/my-app/src/module/components/AnimalCreate.tsx
@@ -1,138 +1,122 @@
 import {AnimalGender, AnimalType} from "../types";
-import { Formik, Form, Field } from 'formik';
+import {Formik, Form, Field} from 'formik';
 import * as Yup from "yup";
+import React from "react";
 
-export function AnimalCreate() {
+const genders = [AnimalGender.FEMALE, AnimalGender.MALE];
+const types = [AnimalType.CAT, AnimalType.DOG];
+const dateNow: string = new Date().toISOString().substr(0, 10);
 
-    const genders = [AnimalGender.FEMALE, AnimalGender.MALE];
-    const types = [AnimalType.CAT, AnimalType.DOG];
-    const dateNow: string = new Date().toISOString().substr(0, 10);
+const SignupSchema = Yup.object().shape({
+    name: Yup.string()
+        .min(2, 'Слишком короткое!')
+        .max(50, 'Слишком длинное!')
+        .required('Обязательное поле'),
+    breed: Yup.string()
+        .min(2, 'Слишком короткое!')
+        .max(50, 'Слишком длинное!')
+        .required('Обязательное поле'),
+    type: Yup.string().required('Обязательное поле'),
+    gender: Yup.string().required('Обязательное поле'),
+    birthday: Yup.date().max(dateNow, 'Некорректное поле'),
+});
 
-    const SignupSchema = Yup.object().shape({
-        firstName: Yup.string()
-            .min(2, 'Too Short!')
-            .max(50, 'Too Long!')
-            .required('Required'),
-        lastName: Yup.string()
-            .min(2, 'Too Short!')
-            .max(50, 'Too Long!')
-            .required('Required'),
-        email: Yup.string().email('Invalid email').required('Required'),
-    });
-
-    const ValidationSchemaExample = () => (
-        <div>
-            <h1>Signup</h1>
-            <Formik
-                initialValues={{
-                    firstName: '',
-                    lastName: '',
-                    email: '',
-                }}
-                validationSchema={SignupSchema}
-                onSubmit={values => {
-                    // same shape as initial values
-                    console.log(values);
-                }}
-            > {({ errors, touched }) => (
-                <Form>
-                    <Field name="firstName" />
-                    {errors.firstName && touched.firstName ? (
-                        <div>{errors.firstName}</div>
+export const AnimalCreate = () => (
+    <div>
+        <h1>Новое животное</h1>
+        <Formik
+            initialValues={{
+                name: '',
+                breed: '',
+                type: types[0],
+                gender: genders[0],
+                birthday: dateNow,
+                hobby: '',
+                features: '',
+            }}
+            validationSchema={SignupSchema}
+            onSubmit={values => {
+                console.log(values);
+            }}
+        >
+            {({errors, touched}:any) => (
+            <Form>
+                <div className="form-group">
+                    <label htmlFor="name-input">Кличка</label>
+                    <Field name="name" id="name-input" className="form-control" type="text" placeholder="Кличка"/>
+                    {errors.name && touched.name ? (
+                        <div className="validation">{errors.name}</div>
                     ) : null}
-                    <Field name="lastName" />
-                    {errors.lastName && touched.lastName ? (
-                        <div>{errors.lastName}</div>
-                    ) : null}
-                    <Field name="email" type="email" />
-                    {errors.email && touched.email ? <div>{errors.email}</div> : null}
-                    <button type="submit">Submit</button>
-                </Form>
-            )}
-            </Formik>
-        </div>
-    );
-
-    return (
-        <div>
-            {ValidationSchemaExample}
-            <div className="container">
-                <div className="content">
-                    <form>
-                        <h1>Новое животное</h1>
-                        <div className="form-group">
-                            <label htmlFor="name-input">Кличка</label>
-                            <input id="name-input" className="form-control" type="text" placeholder="Кличка"/>
-                            <div className="validation">
-                                Обязательное поле, не короче 2 символов
-                            </div>
-
-                        </div>
-
-                        <div className="form-group">
-                            <label htmlFor="breed-input">Порода</label>
-                            <input id="breed-input" className="form-control" type="text" placeholder="Порода"
-                                   required/>
-                            <div className="validation">
-                                Обязательное поле, не короче 2 символов
-                            </div>
-                        </div>
-
-                        <div className="form-group">
-                            <label htmlFor="type-select">Тип животного</label>
-                            <select id="type-select" className="form-control" required>
-                                <option>{{types}}</option>
-                            </select>
-                            <div className="validation">
-                                Некорректное поле
-                            </div>
-                        </div>
-
-                        <div className="form-group">
-                            <label htmlFor="gender-select">Пол животного</label>
-                            <select id="gender-select" className="form-control" required>
-                                <option>{{genders}}</option>
-                            </select>
-                            <div className="validation">
-                                Некорректное поле
-                            </div>
-                        </div>
-
-                        <div className="form-group">
-                            <label htmlFor="birthday-input">Дата рождения</label>
-                            <input id="birthday-input" className="form-control" type="date" required/>
-                            <div className="validation">
-                                Некорректное поле
-                            </div>
-                        </div>
-
-                        <div className="form-group">
-                            <label htmlFor="hobby-input">Хобби, интересы </label>
-                            <input id="hobby-input" className="form-control" type="text"
-                                   placeholder="Хобби, интересы"/>
-                            <div className="validation">
-                                Некорректное поле
-                            </div>
-                        </div>
-
-                        <div className="form-group">
-                            <label htmlFor="features-input">Особенности, другие характеристики</label>
-                            <input id="features-input" className="form-control" type="text"
-                                   placeholder="Особенности, другие характеристики"/>
-                            <div className="validation">
-                                Некорректное поле
-                            </div>
-                        </div>
-
-                        {/*<button type="button" className="btn" onClick="addNewAnimal(animal); submitForm(animalsForm)">*/}
-                        {/*    Добавить*/}
-                        {/*</button>*/}
-                        {/*<button type="button" className="btn" onClick="closeForm()">*/}
-                        {/*    Закрыть*/}
-                        {/*</button>*/}
-                    </form>
                 </div>
-            </div>
-        </div>
-    );
-}
+
+                <div className="form-group">
+                    <label htmlFor="breed-input">Порода</label>
+                    <Field name="breed" id="breed-input" className="form-control" type="text" placeholder="Порода"/>
+                    {errors.breed && touched.breed ? (
+                        <div className="validation">{errors.breed}</div>
+                    ) : null}
+                </div>
+
+                <div className="form-group">
+                    <label>Тип животного</label>
+                    <label>
+                        <Field name="type" type="radio" value="types[0]"/>
+                        {types[0]}
+                    </label>
+                    <label>
+                        <Field name="type" type="radio" value="types[1]"/>
+                        {types[1]}
+                    </label>
+                    {errors.type && touched.type ? <div className="validation">{errors.type}</div> : null}
+                </div>
+                <div className="form-group">
+                    <label>Пол животного</label>
+                    <label>
+                        <Field name="gender" type="radio" value="genders[0]"/>
+                        {genders[0]}
+                    </label>
+                    <label>
+                        <Field name="gender" type="radio" value="genders[1]"/>
+                        {genders[1]}
+                    </label>
+                    {errors.gender && touched.gender ? <div className="validation">{errors.gender}</div> : null}
+                </div>
+
+                <div className="form-group">
+                    <label htmlFor="birthday-input">Дата рождения</label>
+                    <Field name="birthday" id="birthday-input" className="form-control" type="date"/>
+                    {errors.birthday && touched.birthday ? (
+                        <div className="validation">{errors.birthday}</div>
+                    ) : null}
+                </div>
+
+                <div className="form-group">
+                    <label htmlFor="hobby-input">Хобби, интересы </label>
+                    <Field name="hobby" id="hobby-input" className="form-control" type="text"
+                           placeholder="Хобби, интересы"/>
+                    {errors.hobby && touched.hobby ? (
+                        <div className="validation">{errors.hobby}</div>
+                    ) : null}
+                </div>
+
+                <div className="form-group">
+                    <label htmlFor="features-input">Особенности</label>
+                    <Field name="features" id="features-input" className="form-control" type="text"
+                           placeholder="Особенности, другие характеристики"/>
+                    {errors.features && touched.features ? (
+                        <div className="validation">{errors.features}</div>
+                    ) : null}
+                </div>
+
+                {/*<button type="button" className="btn" onClick="addNewAnimal(animal); submitForm(animalsForm)">*/}
+                {/*    Добавить*/}
+                {/*</button>*/}
+                {/*<button type="button" className="btn" onClick="closeForm()">*/}
+                {/*    Закрыть*/}
+                {/*</button>*/}
+                <button type="submit">Submit</button>
+            </Form>
+        )}
+        </Formik>
+    </div>
+);
